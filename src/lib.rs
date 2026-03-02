b4f7ec4fafea31679a687786b62a49929e424cf8
DominicDern <74207434+DominicDern@users.noreply.github.com>
2026-03-01 23:07:57 -0400

    FIxed bug where bounds on die rolls weren't able to crit
    just a bounds issue

--- src/lib.rs
+++ src/lib.rs
@@ -24,14 +24,14 @@ struct MonoRoll {
 impl MonoRoll {
     fn roll(&self) -> (u16, String) {
         let roll: u16 = match &self.dice {
-            Die::D2 => rand::random_range(1..2),
-            Die::D4 => rand::random_range(1..4),
-            Die::D6 => rand::random_range(1..6),
-            Die::D8 => rand::random_range(1..8),
-            Die::D10 => rand::random_range(1..10),
-            Die::D12 => rand::random_range(1..12),
-            Die::D20 => rand::random_range(1..20),
-            Die::Percentile => rand::random_range(1..100),
+            Die::D2 => rand::random_range(1..=2),
+            Die::D4 => rand::random_range(1..=4),
+            Die::D6 => rand::random_range(1..=6),
+            Die::D8 => rand::random_range(1..=8),
+            Die::D10 => rand::random_range(1..=10),
+            Die::D12 => rand::random_range(1..=12),
+            Die::D20 => rand::random_range(1..=20),
+            Die::Percentile => rand::random_range(1..=100),
         };
         (roll, self.dmg_type.clone())
     }
@@ -43,6 +43,7 @@ mod tests {
 
     #[test]
     fn it_works() {
+
         // let result = add(2, 2);
         // assert_eq!(result, 4);
     }
